9433a310caf3ba79b813fed9c31906951e399c6f
deo1 <deotsone@gmail.com>
2015-07-25 23:40:56 -0700

    adding demonstrative assignment case for list methods

--- 05Lists.py
+++ 05Lists.py
@@ -2,6 +2,7 @@ __author__ = 'jbowman'
 
 # Lists
 l = ['Drake', 'Derp', 'Derek', 'Dominique']
+p = []
 
 print(l)
 print(l[0])
@@ -9,11 +10,16 @@ print(l[1])
 print(l[2])
 
 # add and remove
+print("-------")
 l.append('Victoria')
 print(l)
 l.remove('Derp')
 l.remove('Drake')
 print(l)
+p = l.append('Drake')
+l.append('Drake')
+print(l)
+print(p)
 
 # sort
 l.append('Drake')
